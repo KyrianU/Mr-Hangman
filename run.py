9f14e2c48104fd8541ed9d1f94d5df8e4c030f3e
KyrianU <kyrian.uwaegbulem@gmail.com>
2022-06-16 15:01:57 +0000

    add game_end function to give the user chance to play again or quit the game

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 import string
 from words import words
+from gallows import hangman_stages
 
 
 def get_random_word(words):
@@ -21,7 +22,7 @@ def player_name():
     while True:
         name = input('Please enter your name: \n')
         if name.isalpha():
-            print(f'Welcome to the game {name}, best of luck')
+            print(f'Welcome to the game {name}, best of luck! \n')
             start_game()
             break
         else:
@@ -36,14 +37,14 @@ def game_rules():
        """
        Welcome to Hangman!
        The rules are very simple, you are given a random word,
-       each unsuccessful attempts at guessing a letter adds a new body 
+       each unsuccessful attempts at guessing a letter adds a new body
        part to the gallows. After 6 uncessful attempts the player will be
        hunged and the game is over. To win the game, the player has to guess
        the full word within the allocated 6 attempts.
 
        Best of luck!
        """
-        
+      
     )
 
 
@@ -56,7 +57,7 @@ def menu():
         print('Press 1 to start the game')
         print('press 2 to for the rules of the game')
         print('press 3 to exit the game \n')
-        choice = input('Choose one of the menu options: ')
+        choice = input('Choose one of the menu options: \n')
         if choice == '1':
             menu_choices = False
             player_name()
@@ -91,6 +92,24 @@ def start_game():
             print("Invalid input, please choose between 'Y' or 'N' ")
 
 
+def game_end():
+    """
+    End of the game option
+    """
+    while True:
+        print('Would you like to play again?')
+        play_again = input("Press 'Y for Yes or 'N' for No \n")
+
+        if play_again == 'Y':
+            play_game()
+            break
+        elif play_again == 'N':
+            print('Thanks for playing. See you soon...\n')
+            exit()
+        else:
+            print("Invalid input, please chose between 'Y' or 'N \n")
+
+
 def play_game():
     """
     Starts the game for user, a random word
@@ -111,6 +130,7 @@ def play_game():
     while len(letters_in_word) and attempts > 0:
         print(" Letters Guessed: ", " ".join(letters_guessed))
         print(f'attempts remaining: {attempts}')
+        print(hangman_stages(attempts))
         guess = input('Please pick a letter: \n').upper()
 
         if guess in alpha - letters_guessed:
@@ -127,11 +147,12 @@ def play_game():
             print('Invalid character, please choose letters only')
 
     if attempts == 0:
+        print(hangman_stages(attempts))
         print(f'You have lost. the hidden word was {word}')
-        start_game()
+        game_end()
     else:
         print('congrats')
-        start_game()
+        game_end()
 
 
 if __name__ == "__main__":
